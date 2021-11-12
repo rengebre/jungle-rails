2386b41b9f35711bb951e647ecca00370303b61b
Russell Engebretson <russell.engebretson@gmail.com>
2021-11-12 02:22:06 +0000

    Moved admin username and password to .env file

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "Book"
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
+    # puts ENV['ADMIN_USERNAME']
     @product_count = Product.count(:all)
     @category_count = Category.count(:all)
   end
