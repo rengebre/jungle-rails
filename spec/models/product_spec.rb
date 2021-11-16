14a8915d5561562de4b1e6bf01352e48458ab77c
Russell Engebretson <russell.engebretson@gmail.com>
2021-11-16 23:28:51 +0000

    add User rspec tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,25 +12,7 @@ RSpec.describe Product, type: :model do
       expect(@product.id).to be_present
     end
 
-    it "should save successfully" do
-      @category = Category.new(name: "Test")
-      @product = Product.new(  name: "Timothy", price: nil, quantity: 10, category: @category)
-
-      @product.save
-
-      expect(@product.errors.full_messages).to include("Price can't be blank")
-    end
-
-    it "should save successfully" do
-      @category = Category.new(name: "Test")
-      @product = Product.new(  name: "Timothy", price: 1020, quantity: nil, category: @category)
-
-      @product.save
-
-      expect(@product.errors.full_messages).to include("Quantity can't be blank")
-    end
-
-    it "should save successfully" do
+    it "should not save if name is nil" do
       @category = Category.new(name: "Test")
       @product = Product.new(  name: nil, price: 1020, quantity: 10, category: @category)
 
@@ -39,7 +21,25 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
-    it "should save successfully" do
+    it "should not save if price is nil" do
+      @category = Category.new(name: "Test")
+      @product = Product.new(  name: "Timothy", price: nil, quantity: 10, category: @category)
+
+      @product.save
+
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it "should not save if quantity is nil" do
+      @category = Category.new(name: "Test")
+      @product = Product.new(  name: "Timothy", price: 1020, quantity: nil, category: @category)
+
+      @product.save
+
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it "should not save if category is nil" do
       @category = Category.new(name: "Test")
       @product = Product.new(  name: "Timothy", price: 1020, quantity: 10, category: nil)
 
